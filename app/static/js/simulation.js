4f0344fc3c9d9f5524f58489c83cd53d75969687
ZHOU FENGYI <zhou0200@DMAL-admins-MacBook-Pro.local>
2017-05-03 21:22:02 +0800

    simulation page

--- app/static/js/simulation.js
+++ app/static/js/simulation.js
@@ -68,3 +68,197 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+
+// control parameters setting
+            $(function() {
+                var option = $('#form-control_1_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_1_1').prop('disabled',true);
+                     $('#form-control_1_2').prop('disabled',true);
+                     $('#form-control_1_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_1_1').prop('disabled',false);
+                     $('#form-control_1_2').prop('disabled',false);
+                     $('#form-control_1_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_1_4").change(function() {
+                    var option = $('#form-control_1_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_1_1').prop('disabled',false);
+                     $('#form-control_1_2').prop('disabled',false);
+                     $('#form-control_1_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_1_1').prop('disabled',true);
+                     $('#form-control_1_2').prop('disabled',true);
+                     $('#form-control_1_3').prop('disabled',true);
+                    }                });
+            });
+
+            $(function() {
+                var option = $('#form-control_2_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_2_1').prop('disabled',true);
+                     $('#form-control_2_2').prop('disabled',true);
+                     $('#form-control_2_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_2_1').prop('disabled',false);
+                     $('#form-control_2_2').prop('disabled',false);
+                     $('#form-control_2_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_2_4").change(function() {
+                    var option = $('#form-control_2_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_2_1').prop('disabled',false);
+                     $('#form-control_2_2').prop('disabled',false);
+                     $('#form-control_2_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_2_1').prop('disabled',true);
+                     $('#form-control_2_2').prop('disabled',true);
+                     $('#form-control_2_3').prop('disabled',true);
+                    }                });
+            });
+
+            $(function() {
+                var option = $('#form-control_3_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_3_1').prop('disabled',true);
+                     $('#form-control_3_2').prop('disabled',true);
+                     $('#form-control_3_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_3_1').prop('disabled',false);
+                     $('#form-control_3_2').prop('disabled',false);
+                     $('#form-control_3_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_3_4").change(function() {
+                    var option = $('#form-control_3_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_3_1').prop('disabled',false);
+                     $('#form-control_3_2').prop('disabled',false);
+                     $('#form-control_3_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_3_1').prop('disabled',true);
+                     $('#form-control_3_2').prop('disabled',true);
+                     $('#form-control_3_3').prop('disabled',true);
+                    }                });
+            });
+
+            $(function() {
+                var option = $('#form-control_4_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_4_1').prop('disabled',true);
+                     $('#form-control_4_2').prop('disabled',true);
+                     $('#form-control_4_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_4_1').prop('disabled',false);
+                     $('#form-control_4_2').prop('disabled',false);
+                     $('#form-control_4_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_4_4").change(function() {
+                    var option = $('#form-control_4_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_4_1').prop('disabled',false);
+                     $('#form-control_4_2').prop('disabled',false);
+                     $('#form-control_4_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_4_1').prop('disabled',true);
+                     $('#form-control_4_2').prop('disabled',true);
+                     $('#form-control_4_3').prop('disabled',true);
+                    }                });
+            });
+
+            $(function() {
+                var option = $('#form-control_5_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_5_1').prop('disabled',true);
+                     $('#form-control_5_2').prop('disabled',true);
+                     $('#form-control_5_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_5_1').prop('disabled',false);
+                     $('#form-control_5_2').prop('disabled',false);
+                     $('#form-control_5_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_5_4").change(function() {
+                    var option = $('#form-control_5_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_5_1').prop('disabled',false);
+                     $('#form-control_5_2').prop('disabled',false);
+                     $('#form-control_5_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_5_1').prop('disabled',true);
+                     $('#form-control_5_2').prop('disabled',true);
+                     $('#form-control_5_3').prop('disabled',true);
+                    }                });
+            });
+
+            $(function() {
+                var option = $('#form-control_6_4').find(":selected").text();
+                if(option=='Auto'){
+                     $('#form-control_6_1').prop('disabled',true);
+                     $('#form-control_6_2').prop('disabled',true);
+                     $('#form-control_6_3').prop('disabled',true);
+
+                }
+                else{
+                     $('#form-control_6_1').prop('disabled',false);
+                     $('#form-control_6_2').prop('disabled',false);
+                     $('#form-control_6_3').prop('disabled',false);
+                }
+
+            });
+
+            $(function() {
+                $("#form-control_6_4").change(function() {
+                    var option = $('#form-control_6_4').find(":selected").text();
+                    if(option=='Manual'){
+                     $('#form-control_6_1').prop('disabled',false);
+                     $('#form-control_6_2').prop('disabled',false);
+                     $('#form-control_6_3').prop('disabled',false);
+
+                    }
+                    else{
+                     $('#form-control_6_1').prop('disabled',true);
+                     $('#form-control_6_2').prop('disabled',true);
+                     $('#form-control_6_3').prop('disabled',true);
+                    }                });
+            });
