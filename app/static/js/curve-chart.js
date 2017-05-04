4472c4b61df50f04c6763b1f3b3000c1c850738d
ZHOU FENGYI <zhou0200@DMAL-admins-MacBook-Pro.local>
2017-05-04 14:04:39 +0800

    monitoring page update

--- app/static/js/curve-chart.js
+++ app/static/js/curve-chart.js
@@ -1,6 +1,6 @@
-var curve_margin = {top: 20, right: 10, bottom: 30, left: 60},
-    curve_width = 500 - curve_margin.left - curve_margin.right,
-    curve_height = 220 - curve_margin.top - curve_margin.bottom;
+var curve_margin = {top: 10, right: 20, bottom: 30, left: 70},
+    curve_width = 550 - curve_margin.left - curve_margin.right,
+    curve_height = 190 - curve_margin.top - curve_margin.bottom;
 
 var parseTime = d3.timeParse("%d-%m-%y %H:%M");
 
